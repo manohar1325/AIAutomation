f5f8341f68ab1a5a7ad8213c51d551d3ca00c925
manohar1325 <manoharmanu1325@gmail.com>
2026-06-24 21:06:17 +0530

    added logs for secrets

--- config/env.ts
+++ config/env.ts
@@ -1,6 +1,11 @@
-import * as dotenv from 'dotenv'
+import * as dotenv from 'dotenv';
 
 dotenv.config();
+
+console.log("===== ENV DEBUG =====");
+console.log("BASE_URL =", process.env.BASE_URL);
+console.log("API_KEY EXISTS =", !!process.env.API_KEY);
+console.log("=====================");
 
 export const config = {
     apiKey: process.env.API_KEY || '',
